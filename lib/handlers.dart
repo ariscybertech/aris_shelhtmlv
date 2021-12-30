afe6a40953f9a57367cd03b731b934e2b137c57f
ariscybertech <ariscybertech@gmail.com>
2021-12-30 19:06:41 +0500

    first commit

---
+++ lib/handlers.dart
@@ -0,0 +1,11 @@
+library shelf_html_example.frontend.handlers;
+
+import 'dart:html';
+
+import 'package:shelf/shelf.dart';
+
+import 'components/app.dart';
+import 'components/card.dart';
+
+part 'src/handlers/sign_in_handler.dart';
+part 'src/handlers/card_handler.dart';
